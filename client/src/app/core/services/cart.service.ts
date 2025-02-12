515f276b5bd2e41f9f5c0cc297620497395431ef
zezinh0 <ze_manuel_costa1998@hotmail.com>
2025-02-12 15:48:31 +0000

    Improved the Add and Remove Items From Cart. Crate a Simple Order Summary

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -15,6 +15,21 @@ export class CartService {
   itemCount = computed(() => {
     return this.cart()?.items.reduce((sum,item) => sum + item.quantity, 0)
   })
+  totals = computed(() => {
+    const cart = this.cart();
+    if(!cart) return null;
+
+    const subtotal = cart.items.reduce((sum,item) => sum + item.price * item.quantity, 0);
+    const shipping = 0;
+    const discount = 0;
+
+    return {
+      subtotal,
+      shipping,
+      discount,
+      total: subtotal + shipping - discount
+    }
+  })
 
   getCart(id: string)
   {
@@ -43,6 +58,36 @@ export class CartService {
     cart.items = this.addOrUpdateItem(cart.items, item, quantity);
     this.setCart(cart);
   } 
+
+  removeItemFromCart(productId: number, quantity = 1) {
+    const cart = this.cart();
+    if(!cart) return;
+    
+    const index = cart.items.findIndex(x => x.productId === productId);
+
+    if(index !== -1){
+      if(cart.items[index].quantity > quantity){
+        cart.items[index].quantity -= quantity;
+      }else{
+        cart.items.splice(index, 1);
+      }
+
+      if(cart.items.length === 0){
+        this.deleteCart();
+      }else{
+        this.setCart(cart)
+      }
+    }
+  }
+
+  deleteCart() {
+    this.http.delete(this.baseUrl + 'cart?id=' + this.cart()?.id).subscribe({
+      next: () => {
+        localStorage.removeItem('cart_id');
+        this.cart.set(null);
+      }
+    })
+  }
 
   private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
     const index = items.findIndex(p => p.productId === item.productId);
--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -8,6 +8,8 @@ import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { MatDivider } from '@angular/material/divider';
+import { CartService } from '../../../core/services/cart.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
@@ -19,6 +21,7 @@ import { MatDivider } from '@angular/material/divider';
     MatInput, 
     MatLabel,
     MatDivider,
+    FormsModule,
   ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
@@ -26,7 +29,10 @@ import { MatDivider } from '@angular/material/divider';
 export class ProductDetailsComponent implements OnInit{
   private shopService = inject(ShopService);
   private activatedRoute = inject(ActivatedRoute);
+  private cartService = inject(CartService);
   product?: Product;
+  quantityInCart = 0;
+  quantity = 1;
 
   ngOnInit(): void {
     this.loadProduct();
@@ -37,8 +43,35 @@ export class ProductDetailsComponent implements OnInit{
     if(!id) return;
 
     this.shopService.getProduct(+id).subscribe({
-      next: product => this.product = product,
+      next: product =>{
+        this.product = product;
+        this.updateQuantityInCart();
+      } ,
       error: error => console.error(error)
     })
+  }
+
+  updateCart(){
+    if(!this.product) return;
+
+    if (this.quantity > this.quantityInCart){
+      const itemsToAdd = this.quantity - this.quantityInCart;
+      this.quantityInCart += itemsToAdd;
+      this.cartService.addItemToCart(this.product, itemsToAdd);
+    }else{
+      const itemsToRemove = this.quantityInCart - this.quantity;
+      this.quantityInCart -= itemsToRemove;
+      this.cartService.removeItemFromCart(this.product.id, itemsToRemove);
+    }
+
+  }
+
+  updateQuantityInCart(){
+    this.quantityInCart = this.cartService.cart()?.items.find(x => x.productId === this.product?.id)?.quantity || 0;
+    this.quantity = this.quantityInCart || 1;
+  }
+
+  getButtonText(){
+    return this.quantityInCart > 0 ? 'Update cart' : 'Add to cart'
   }
 }
